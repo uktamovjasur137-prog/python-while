c7083c4510d6ca269c33a997b94b270b5c4963fb
Jasur <uktamovjasur137@gmail.com>
2026-03-24 14:46:41 +0500

    done

---
+++ task02.py
@@ -0,0 +1,9 @@
+password = input("Parol: ")
+
+while True:
+    if password != "python123":
+        print("xato")
+        password = input("yana kiriting: ")
+    elif password == "python123":
+        print("togri")
+        break
