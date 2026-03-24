d46945c94164ea29885d7d31b87b502d26ffaf42
Jasur <uktamovjasur137@gmail.com>
2026-03-24 15:36:13 +0500

    done

---
+++ task05.py
@@ -0,0 +1,13 @@
+total = 0
+
+while True:
+    text = input("Text: ")
+
+    if text == "stop":
+        print(f"Umumiy matnlar soni: {total}")
+        break
+
+    else:
+        total += 1
+         
+
